8142b0bb6ed18cbc429b645f3c2805f8275e50e4
Brian Whitton <me@brianwhitton.com>
2017-03-12 14:38:07 -0400

    adds an array helper

---
+++ app/helpers/array.js
@@ -0,0 +1,1 @@
+export { default, array } from 'nypr-ads/helpers/array';
---
+++ tests/unit/helpers/array-test.js
@@ -0,0 +1,12 @@
+
+import { array } from 'dummy/helpers/array';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | array');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = array([42, 54]);
+  assert.ok(result);
+  assert.equal(results, [42, 54], 'returns params in an array')
+});
