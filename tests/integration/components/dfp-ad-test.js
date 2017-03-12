f428bf1220effe766d1c60edebfb24e6e8b1f299
Brian Whitton <me@brianwhitton.com>
2017-03-12 14:38:30 -0400

    test dfp display component

--- tests/integration/components/dfp-ad-test.js
+++ tests/integration/components/dfp-ad-test.js
@@ -1,25 +1,65 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { moduleForComponent } from 'ember-qunit';
+import test from 'ember-sinon-qunit/test-support/test';
 import hbs from 'htmlbars-inline-precompile';
+import googletag from 'googletag';
 
 moduleForComponent('dfp-ad', 'Integration | Component | dfp ad', {
-  integration: true
+  integration: true,
+  beforeEach() {
+    googletag.pubads = googletag.pubads || (() => {});
+  }
 });
 
 test('it renders', function(assert) {
+  this.render(hbs`{{dfp-ad target='foo'}}`);
 
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+  assert.equal(this.$('#foo').length, 1);
+});
 
-  this.render(hbs`{{dfp-ad}}`);
+test('it initializes a basic display ad', function(assert) {
+  this.stub(googletag.cmd, 'push', f => f());
+  
+  let defineMock = this.mock(googletag)
+    .expects('defineSlot')
+    .once()
+    .returns({ addService: this.mock().once() });
+  let displayMock = this.mock(googletag).expects('display').once();
+  
+  this.render(hbs`{{dfp-ad target='foo target' sizes=(array 100 200 300) slot='foo slot'}}`);
+  
+  assert.ok(displayMock.firstCall.calledWith('foo target'));
+  assert.ok(defineMock.firstCall.calledWith('foo slot', [100, 200, 300], 'foo target'));
+});
 
-  assert.equal(this.$().text().trim(), '');
+test('it initializes an ad with a size mapping', function(assert) {
+  this.stub(googletag.cmd, 'push', f => f());
+  
+  this.mock(googletag).expects('display').once();
+  this.stub(googletag, 'defineSlot')
+    .returns({
+      addService: this.stub,
+      defineSizeMapping: this.mock().once()
+    });
+  
+  this.mock(googletag)
+    .expects('sizeMapping')
+    .once()
+    .returns({
+      build: this.mock().once(),
+      addSize: this.mock().twice()
+    });
 
-  // Template block usage:
-  this.render(hbs`
-    {{#dfp-ad}}
-      template block text
-    {{/dfp-ad}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  this.mock(window)
+    .expects('matchMedia')
+    .twice()
+    .returns({
+      addListener: this.mock().twice(),
+      removeListener() {}
+    })
+  
+  this.render(hbs`{{dfp-ad target='foo target' sizes=(array 100 200 300) slot='foo slot' mapping=(array
+    (array 100 (array 200 300))
+    (array 300 (array 400 500))
+  )}}`);
+  
 });
