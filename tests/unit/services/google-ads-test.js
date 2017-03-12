8c96a20a7e94d99189cd9f8e613e5c160b6628fd
Brian Whitton <me@brianwhitton.com>
2017-03-12 12:31:05 -0400

    adds tests for targeting

--- tests/unit/services/google-ads-test.js
+++ tests/unit/services/google-ads-test.js
@@ -1,4 +1,6 @@
-import { moduleFor, test } from 'ember-qunit';
+import { moduleFor } from 'ember-qunit';
+import test from 'ember-sinon-qunit/test-support/test';
+import googletag from 'googletag';
 
 moduleFor('service:google-ads', 'Unit | Service | google ads', {
   // Specify the other units that are required for this test.
@@ -9,4 +11,25 @@ moduleFor('service:google-ads', 'Unit | Service | google ads', {
 test('it exists', function(assert) {
   let service = this.subject();
   assert.ok(service);
+});
+
+test('it calls setTargeting on passed in hash', function(assert) {
+  googletag.pubads = googletag.pubads || (() => {});
+  
+  let testTargets = {tags: ['foo', 'bar'], show: 'baz'};
+  let targetingMock = this.mock().exactly(5);
+
+  this.stub(googletag.cmd, 'push', f => f());
+  this.stub(googletag, 'pubads', () => ({
+    setTargeting: targetingMock
+  }));
+  
+  let service = this.subject();
+  service.doTargeting(testTargets);
+  targetingMock.verify();
+  assert.ok(targetingMock.getCall(0).calledWith('url', window.location.pathname));
+  assert.ok(targetingMock.getCall(1).calledWith('host', window.location.host));
+  assert.ok(targetingMock.getCall(2).calledWith('fullurl', window.location));
+  assert.ok(targetingMock.getCall(3).calledWith('tags', ['foo', 'bar']));
+  assert.ok(targetingMock.getCall(4).calledWith('show', 'baz'));
 });
