4c56fa612ba10d45a25577531358981e31cf33e2
Brian Whitton <me@brianwhitton.com>
2017-03-17 10:35:09 -0400

    typo

--- addon/components/dfp-ad.js
+++ addon/components/dfp-ad.js
@@ -67,6 +67,6 @@ export default Component.extend({
     if (mqList && mqList.length) {
       mqList.forEach(mq => mq.removeListener(bind(this, 'refresh')));
     }
-    googletag.cms.push(() => googletag.destroySlots([ad]));
+    googletag.cmd.push(() => googletag.destroySlots([ad]));
   }
 });
