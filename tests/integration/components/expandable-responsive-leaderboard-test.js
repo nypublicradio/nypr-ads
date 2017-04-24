fdbe29c7bda01c2a67fcd532ad5a3da1bdcd349a
Brian Whitton <me@brianwhitton.com>
2017-04-24 11:13:03 -0400

    adds example usage tests

--- tests/integration/components/expandable-responsive-leaderboard-test.js
+++ tests/integration/components/expandable-responsive-leaderboard-test.js
@@ -1,8 +1,13 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { moduleForComponent } from 'ember-qunit';
+import test from 'ember-sinon-qunit/test-support/test';
 import hbs from 'htmlbars-inline-precompile';
+import googletag from 'googletag';
 
 moduleForComponent('expandable-responsive-leaderboard', 'Integration | Component | expandable responsive leaderboard', {
-  integration: true
+  integration: true,
+  beforeEach() {
+    googletag.pubads = googletag.pubads || (() => {});
+  }
 });
 
 test('it renders', function(assert) {
@@ -13,4 +18,66 @@ test('it renders', function(assert) {
   this.render(hbs`{{expandable-responsive-leaderboard}}`);
 
   assert.equal(this.$('.leaderboard').length, 1);
+});
+
+test('large breakpoint', function(/*assert*/) {
+  this.stub(googletag.cmd, 'push').callsFake(f => f());
+  
+  this.mock(googletag).expects('display').once();
+  this.mock(googletag).expects('defineSlot')
+    .once()
+    .returns({
+      addService: this.stub,
+      defineSizeMapping: this.mock().atLeast(1)
+    });
+  
+  this.mock(googletag)
+    .expects('sizeMapping')
+    .once()
+    .returns({
+      build: this.mock().atLeast(1),
+      addSize: this.mock().atLeast(2)
+    });
+
+  this.mock(window)
+    .expects('matchMedia')
+    .twice()
+    .returns({
+      addListener: this.mock().atLeast(2),
+      removeListener() {}
+    });
+  
+  this.render(hbs`
+    {{expandable-responsive-leaderboard
+      site='wqxr'
+      smallBreakpoint=false
+    }}
+  `);
+});
+
+test('small breakpoint', function(/*assert*/) {
+  this.stub(googletag.cmd, 'push').callsFake(f => f());
+  
+  this.mock(googletag).expects('display').once();
+  this.mock(googletag).expects('defineSlot')
+    .once()
+    .returns({
+      addService: this.stub,
+      defineSizeMapping: this.mock().never()
+    });
+  
+  this.mock(googletag)
+    .expects('sizeMapping')
+    .never();
+
+  this.mock(window)
+    .expects('matchMedia')
+    .never();
+  
+  this.render(hbs`
+    {{expandable-responsive-leaderboard
+      site='wqxr'
+      smallBreakpoint=true
+    }}
+  `);
 });
