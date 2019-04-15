95947a43b3ab8cf64593efca726e1b9805c28f4e
walsh9 <walsh9@gmail.com>
2019-04-15 12:43:20 -0400

    Update component to handle property changes

--- addon/components/do-targeting.js
+++ addon/components/do-targeting.js
@@ -3,25 +3,39 @@ import { doTargeting, clearTargeting } from 'nypr-ads';
 
 export default Component.extend({
   tagName: '',
-  didInsertElement() {
+  _clearTargeting() {
+    if (this.prevKey) {
+      clearTargeting(this.prevKey)
+      delete this.prevKey
+    }
+    if (this.prevTargets) {
+      clearTargeting(...Object.keys(this.prevTargets))
+      delete this.prevTargets
+    }
+  },
+  _updateTargeting() {
     let key = this.get('key');
     let value = this.get('value');
     let targets = this.get('targets');
+
+    this._clearTargeting();
+
     if (key && value != null) {
       doTargeting({[key]: value});
+      this.prevKey = key;
     }
     if(targets) {
       doTargeting(targets);
+      this.prevTargets = targets;
     }
   },
+  didInsertElement() {
+    this._updateTargeting();
+  },
+  didUpdateAttrs() {
+    this._updateTargeting();
+  },
   willDestroyElement() {
-    let key = this.get('key');
-    let targets = this.get('targets');
-    if (key) {
-      clearTargeting(key);
-    }
-    if(targets) {
-      clearTargeting(...Object.keys(targets));
-    }
+    this._clearTargeting();
   }
 });
