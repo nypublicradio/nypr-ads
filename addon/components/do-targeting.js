b4304a4e32c9916040f1ae5c02bf67272ef5bd60
walsh9 <walsh9@gmail.com>
2019-04-16 12:52:37 -0400

    Document components and functions

--- addon/components/do-targeting.js
+++ addon/components/do-targeting.js
@@ -1,7 +1,48 @@
 import Component from '@ember/component';
 import { doTargeting, clearTargeting } from 'nypr-ads';
 
+
+/**
+  An invisible component that sets page-level
+  GPT key-value targeting when created and clears
+  it when destroyed.
+
+  Setting one key-value pair:
+  ```html
+    <DoTargeting @key='foo' @value='bar'/>
+  ```
+
+  Setting multiple key-value pairs:
+  ```html
+    <DoTargeting @targets={{hash
+      key1='value1'
+      key2='value2'
+    }} />
+  ```
+
+  @class DoTargeting
+*/
 export default Component.extend({
+/**
+  The name of a value to assign to @key
+
+  @argument key
+  @type {String}
+*/
+/**
+  A value to assign to @key.
+
+  @argument value
+  @type {String|String[]}
+*/
+/**
+  A map of key value pairs
+
+  `{key1:'value1', key2:'value2'}`
+
+  @argument targets
+  @type {Object}
+*/
   tagName: '',
   _clearTargeting() {
     if (this.prevKey) {
--- addon/index.js
+++ addon/index.js
@@ -3,20 +3,50 @@ import { get } from '@ember/object';
 import googletag from 'googletag';
 
 
-export function doTargetingForPath(location=window.location) {
-  doTargeting({
-    'host': location.host,
-    'url': location.pathname,
-    'urlSegments': location.pathname && location.pathname.split('/').filter(segment => segment.length > 0)
-  });
+/**
+  Set targeting for the current 'url', 'host', and 'urlSegments'.
+
+  @function doTargetingForPath
+  @export named
+  @param {Object} [location=window.location] The browser location object or a testable replacement.
+*/
+export function doTargetingForPath(location=window && window.location) {
+  if (location) {
+    doTargeting({
+      'host': location.host,
+      'url': location.pathname,
+      'urlSegments': location.pathname && location.pathname.split('/').filter(segment => segment.length > 0)
+    });
+  }
 }
 
+/**
+  Clear targeting for the current 'url', 'host', and 'urlSegments'.
+
+  @function clearTargetingForPath
+  @export named
+*/
 export function clearTargetingForPath() {
   clearTargeting('host');
   clearTargeting('url');
   clearTargeting('urlSegments');
 }
 
+/**
+  Set targeting for passed in Ember Data Models that
+  have an adTargeting property set up.
+
+  Accepts any number of Ember Data models as parameters.
+
+  Usage:
+  ```js
+    doTargetingForModels(model1, model2);
+  ```
+
+  @function doTargetingForModels
+  @export named
+  @param {DS.Model} ...models Any number of Ember Data models with 'adTargeting' properties.
+*/
 export function doTargetingForModels(...models) {
   models.forEach(model => {
     let targets = {};
@@ -30,6 +60,21 @@ export function doTargetingForModels(...models) {
   });
 }
 
+/**
+  Clear targeting for Ember Data Models that
+  have an adTargeting property set up.
+
+  Accepts any number of Ember Data models as parameters.
+
+  Usage:
+  ```js
+    clearTargetingForModels(model1, model2);
+  ```
+
+  @function clearTargetingForModels
+  @export named
+  @param {DS.Model} ...models Any number of Ember Data models with 'adTargeting' properties.
+*/
 export function clearTargetingForModels(...models) {
   models.forEach(model => {
     let adTargetingMap = get(model, 'adTargeting');
@@ -41,6 +86,18 @@ export function clearTargetingForModels(...models) {
   });
 }
 
+/**
+  Set targeting for a set of key value pairs.
+
+  Usage:
+  ```js
+    doTargeting({key1:'value1', key2:'value2'});
+  ```
+
+  @function doTargeting
+  @export named
+  @param {Object} targets An object containing key value pairs for targeting.
+*/
 export function doTargeting(targets) {
   googletag.cmd.push(function() {
     if (targets) {
@@ -53,6 +110,20 @@ export function doTargeting(targets) {
   });
 }
 
+/**
+  Clear targeting for a set of keys.
+
+  Accepts any number of strings as parameters.
+
+  Usage:
+  ```js
+    clearTargeting('key1', 'key2');
+  ```
+
+  @function clearTargeting
+  @export named
+  @param {String} ...targets Any number key names to clear.
+*/
 export function clearTargeting(...targets) {
   targets.forEach(target => {
     googletag.cmd.push(function() {
@@ -60,3 +131,4 @@ export function clearTargeting(...targets) {
     });
   });
 }
+
